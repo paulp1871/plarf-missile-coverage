eda7cc81eb420f5b2e54b73da27d9be0041a02b8
Paul Pham <paulpham1871@gmail.com>
2026-01-25 02:42:38 -0700

    Made some more backend functionality

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+morgan.token('post', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+const logger = morgan((tokens, req, res) => {
+    const tiny = [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms'
+        ]
+    if (req.method !== 'POST') {
+        return tiny.join(' ')
+    }
+    return tiny.concat(tokens.post(req, res)).join(' ')
+})
+
+app.use(logger)
 
 app.get('/', (req, res) => {
     res.sendFile('output/map.html' , { root : __dirname})
